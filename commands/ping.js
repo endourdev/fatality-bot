eedc5f1a7be86dad4e2477bbbab135355997d6e7
fofhgit <ouaoudamohamed448@gmail.com>
2022-11-28 20:05:48 +0100

    Update

--- commands/ping.js
+++ commands/ping.js
@@ -10,7 +10,6 @@ module.exports = {
         const exampleEmbed = new EmbedBuilder()
     .setColor(0x0099FF)
     .setTitle(`🎮・Fatality Bot Ping\'s`)
-    // .setThumbnail('https://cdn.discordapp.com/avatars/1041409110056382494/3de5e5286ad94e0d3823cdcb6a671e3a.webp?size=128')
     .addFields(
         { name: 'Ping :', value: `Latence : ${sent.createdTimestamp - interaction.createdTimestamp}ms` },
     )
@@ -18,11 +17,5 @@ module.exports = {
     .setFooter({ text: '🎮・Fatality Bot\'s'});
 
     await interaction.editReply({ content: " ", embeds: [exampleEmbed]});
-
-		// interaction.user is the object representing the User who ran the command
-		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		// await interaction.reply(`Cette commande à été faite par ${interaction.user.username}, qui à rejoint le ${interaction.member.joinedAt}.`);
-        // const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
-        // interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
 	},
 };
